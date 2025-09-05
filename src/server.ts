635b205413aad016d76c2eabb682ff9287dda905
Yagho <yaghojp@gmail.com>
2025-09-04 23:05:03 -0300

    feat: configurar ambiente inicial [#1]

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
